c14d1f581e8012034ae71f3b455526751522b2d4
hsadoqi <hsadoqi@gmail.com>
2021-05-12 13:12:58 -0400

    fetch memes and display

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-console.log("loaded")
+// .then(function(res){
+//     res.json()
+// })
+
+// .then((res) => {
+//     res.json()
+// })
+
+// .then(changeResponse)
+
+// function changeResponse(res){
+//     return res.json()
+// }
+
+const BASE_URL = `https://api.imgflip.com/get_memes`
+
+
+function fetchMemes(){
+    fetch(BASE_URL)
+    .then(res => res.json())
+    .then(data => {
+        data.data.memes.forEach(renderMeme)
+    })
+}
+
+
+function renderMeme(meme){
+    const memeContainer = document.getElementById("meme-list")
+    const liTag = document.createElement('li')
+
+    const imageTag = document.createElement('img')
+    imageTag.src = meme.url 
+
+    const pTag = document.createElement('p')
+    pTag.innerText = meme.name 
+
+    liTag.append(imageTag, pTag)
+
+    memeContainer.appendChild(liTag)
+
+}
+
+fetchMemes()
